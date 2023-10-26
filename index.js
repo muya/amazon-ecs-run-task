cf523927ca777ca7e33f7280356b30bddc9f26be
Andrew Sibley <andrew.sibley@field.energy>
2023-10-26 20:11:06 +0100

    Debugging

--- index.js
+++ index.js
@@ -168,6 +168,7 @@ async function run() {
     const matches = taskDefRevisionArn.match(regex);
     core.info(`taskDefRevisionArn was ${taskDefRevisionArn}`);
     core.info(`matches is ${JSON.stringify(matches)}`);
+    core.info(`matches[1] is ${matches[1]}`);
     core.setOutput('task-definition-family-and-revision', matches[1])
 
     const clusterName = cluster ? cluster : 'default';
